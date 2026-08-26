33aaa7ba124d421f0fd43ff3e6c00268a1d02886
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-08-25 21:36:56 -0600

    hacer publica ruta de productos

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const projectController = require('../controllers/project.controller');
 const verifyToken = require('../middleware/auth.middleware');
 
-// Middleware de protección
+// --- RUTA PÚBLICA PARA VER LOS PRODUCTOS Y SUS IMÁGENES ---
+router.get('/', projectController.getProjects);
+
+// --- RUTAS PROTEGIDAS ---
 router.use(verifyToken);
 
 // CREATE
 router.post('/', projectController.createProject);
-
-// READ
-router.get('/', projectController.getProjects);
 
 // UPDATE
 router.put('/:id', projectController.getProjectById);
